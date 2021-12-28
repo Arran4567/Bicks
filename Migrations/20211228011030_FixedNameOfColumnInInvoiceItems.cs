7b5025c581e9a6f6ae35d979142075fe6585b9b2
Arran Jones <Arran4567@gmail.com>
2021-12-28 01:13:52 +0000

    Fixed Foreign Key name for Sale in InvoiceItems

---
+++ Migrations/20211228011030_FixedNameOfColumnInInvoiceItems.cs
@@ -0,0 +1,74 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Bicks.Migrations
+{
+    public partial class FixedNameOfColumnInInvoiceItems : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_InvoiceItems_Sales_SaleInvoiceItems",
+                table: "InvoiceItems");
+
+            migrationBuilder.DropIndex(
+                name: "IX_InvoiceItems_SaleInvoiceItems",
+                table: "InvoiceItems");
+
+            migrationBuilder.DropColumn(
+                name: "SaleInvoiceItems",
+                table: "InvoiceItems");
+
+            migrationBuilder.AddColumn<int>(
+                name: "SaleId",
+                table: "InvoiceItems",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_InvoiceItems_SaleId",
+                table: "InvoiceItems",
+                column: "SaleId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_InvoiceItems_Sales_SaleId",
+                table: "InvoiceItems",
+                column: "SaleId",
+                principalTable: "Sales",
+                principalColumn: "ID",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_InvoiceItems_Sales_SaleId",
+                table: "InvoiceItems");
+
+            migrationBuilder.DropIndex(
+                name: "IX_InvoiceItems_SaleId",
+                table: "InvoiceItems");
+
+            migrationBuilder.DropColumn(
+                name: "SaleId",
+                table: "InvoiceItems");
+
+            migrationBuilder.AddColumn<int>(
+                name: "SaleInvoiceItems",
+                table: "InvoiceItems",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_InvoiceItems_SaleInvoiceItems",
+                table: "InvoiceItems",
+                column: "SaleInvoiceItems");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_InvoiceItems_Sales_SaleInvoiceItems",
+                table: "InvoiceItems",
+                column: "SaleInvoiceItems",
+                principalTable: "Sales",
+                principalColumn: "ID",
+                onDelete: ReferentialAction.Restrict);
+        }
+    }
+}
--- Models/Sale.cs
+++ Models/Sale.cs
@@ -17,7 +17,7 @@ namespace Bicks.Models
         [Display(Name = "Time Of Sale")]
         [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy HH:mm}", ApplyFormatInEditMode = true)]
         public DateTime SaleDateTime { get; set; }
-        [ForeignKey("SaleInvoiceItems")]
+        [ForeignKey("SaleId")]
         [Display(Name = "Invoice Items")]
         public virtual List<InvoiceItem> SaleInvoiceItems { get; set; }
         [ForeignKey("ClientId")]
